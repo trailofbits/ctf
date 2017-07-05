a2a40dcef8537d2cda55ee43f78dbdc0cb267901
Dan Guido <dan@trailofbits.com>
2017-07-05 18:34:43 -0400

    housekeeping

--- git_yolo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "latest version"
-git push origin gh-pages
--- to_gh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# help for https://gist.github.com/chrisjacob/833223
-gitbook build
-cp -rf _book/* ../ctf-pages/
-rm -rf _book
